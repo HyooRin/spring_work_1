794cb9eedbe5bf8de0ee1cdc05f9676b7dd9814d
HyooRin <khl4459@naver.com>
2023-04-07 20:46:25 +0900

    put 방식의 이해

--- demo1/src/main/java/com/example/demo/controller/PostApiController.java
+++ demo1/src/main/java/com/example/demo/controller/PostApiController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,12 @@ public class PostApiController {
 		
 		return reqDto;
 	}
+//	// GET : 주소 : Path
+//	@GetMapping("/chat/{message}")
+//	public String message(@PathVariable String message) {
+//		
+//		
+//		
+//	}
 
 }
